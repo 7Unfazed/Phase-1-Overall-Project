d0c7b8722da8ab4b06c9416dcef34c25197ffaa4
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-04-11 13:36:36 +0300

    creates index.js fetch data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     fetchDataAndPopulateTable();
 });
+function fetchDataAndPopulateTable() {
+    var myUniversityUrl = 'https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json';
+  
+    fetch(myUniversityUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        populateTable(data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+}
